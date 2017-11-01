155b7447216e0792ee03d569b65aea271754c026
Meira Pentermann <meira@meirapentermann.com>
2017-11-01 13:08:06 -0600

    added mapped fields to user

--- JPASDConnect/src/entities/User.java
+++ JPASDConnect/src/entities/User.java
@@ -1,28 +1,41 @@
 package entities;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
 @Entity
 public class User {
 	
-	//field
-
+	//fields
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	
 	private String email;
+	
 	private String password;
+	
 	@Column(name="user_type")
 	private String type;
+	
 	@Column(name="schedule_id")
 	private int scheduleID;
+	
 	@Column(name="cohort_id")
 	private int cohortID;
-
+	
+	@OneToOne(mappedBy="user")
+	private Profile profile;
+	
+	@OneToMany(mappedBy="user")
+	private List<Post> posts;
 	
 	//set and gets
 	public int getId() {
@@ -61,8 +74,19 @@ public class User {
 	public void setCohortID(int cohortID) {
 		this.cohortID = cohortID;
 	}
-	
 
+	public Profile getProfile() {
+		return profile;
+	}
+	public void setProfile(Profile profile) {
+		this.profile = profile;
+	}
+	public List<Post> getPosts() {
+		return posts;
+	}
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
 	//toString
 	@Override
 	public String toString() {
