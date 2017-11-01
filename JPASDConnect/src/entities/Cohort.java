b7d3759804988d634223388d3d21350cb091a5ce
Meira Pentermann <meira@meirapentermann.com>
2017-11-01 13:01:26 -0600

    @Entity

--- JPASDConnect/src/entities/Cohort.java
+++ JPASDConnect/src/entities/Cohort.java
@@ -1,11 +1,14 @@
 package entities;
 
 import java.sql.Date;
+
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
 public class Cohort {
 	
 	@Id
