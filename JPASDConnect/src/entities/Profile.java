a622b4542f52d079a8e0cecb50c870ba14426071
Meira Pentermann <meira@meirapentermann.com>
2017-11-01 15:02:25 -0600

    new fields in profile entity

--- JPASDConnect/src/entities/Profile.java
+++ JPASDConnect/src/entities/Profile.java
@@ -10,112 +10,179 @@ import javax.persistence.OneToOne;
 
 @Entity
 public class Profile {
-	
-	
-	//field
 
+	// field
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-	@Column(name="img_url")
+
+	@Column(name = "img_url")
 	private String img;
-	@Column(name="background_desc")
+
+	@Column(name = "background_desc")
 	private String backgroundDescription;
-	@Column(name="first_name")
+
+	@Column(name = "first_name")
 	private String fname;
-	@Column(name="last_name")
+
+	@Column(name = "last_name")
 	private String lname;
-	@Column(name="prev_industry")
+
+	@Column(name = "prev_industry")
 	private String previousIndustry;
-	@Column(name="coding_exp")
+
+	@Column(name = "coding_exp")
 	private String codingExperience;
-	@Column(name="shirt_size")
+
+	@Column(name = "shirt_size")
 	private String shirtSize;
+
 	private String cohort;
-	@Column(name="user_id")
-	private int userID;
+	
+	@Column(name="website_url")
+	private String websiteUrl;
+	
+	@Column(name="github_url")
+	private String githubUrl;
+	
+	@Column(name="linkedin_url")
+	private String linkedinUrl;
+
 	@OneToOne
-	@JoinColumn(name="user_id")
+	@JoinColumn(name = "user_id")
 	private User user;
-	
-	
-	//gets and sets
+
+	// gets and sets
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getImg() {
 		return img;
 	}
+
 	public void setImg(String img) {
 		this.img = img;
 	}
+
 	public String getBackgroundDescription() {
 		return backgroundDescription;
 	}
+
 	public void setBackgroundDescription(String backgroundDescription) {
 		this.backgroundDescription = backgroundDescription;
 	}
+
 	public String getFname() {
 		return fname;
 	}
+
 	public void setFname(String fname) {
 		this.fname = fname;
 	}
+
 	public String getLname() {
 		return lname;
 	}
+
 	public void setLname(String lname) {
 		this.lname = lname;
 	}
+
 	public String getPreviousIndustry() {
 		return previousIndustry;
 	}
+
 	public void setPreviousIndustry(String previousIndustry) {
 		this.previousIndustry = previousIndustry;
 	}
+
 	public String getCodingExperience() {
 		return codingExperience;
 	}
+
 	public void setCodingExperience(String codingExperience) {
 		this.codingExperience = codingExperience;
 	}
+
 	public String getShirtSize() {
 		return shirtSize;
 	}
+
 	public void setShirtSize(String shirtSize) {
 		this.shirtSize = shirtSize;
 	}
+
 	public String getCohort() {
 		return cohort;
 	}
+
 	public void setCohort(String cohort) {
 		this.cohort = cohort;
 	}
-	public int getUserID() {
-		return userID;
-	}
-	public void setUserID(int userID) {
-		this.userID = userID;
-	}
+
 	public User getUser() {
 		return user;
 	}
+
 	public void setUser(User user) {
 		this.user = user;
 	}
-	
-	//toString
+
+	public String getWebsiteUrl() {
+		return websiteUrl;
+	}
+
+	public void setWebsiteUrl(String websiteUrl) {
+		this.websiteUrl = websiteUrl;
+	}
+
+	public String getGithubUrl() {
+		return githubUrl;
+	}
+
+	public void setGithubUrl(String githubUrl) {
+		this.githubUrl = githubUrl;
+	}
+
+	public String getLinkedinUrl() {
+		return linkedinUrl;
+	}
+
+	public void setLinkedinUrl(String linkedinUrl) {
+		this.linkedinUrl = linkedinUrl;
+	}
+
 	@Override
 	public String toString() {
-		return "Profile [id=" + id + ", img=" + img + ", backgroundDescription=" + backgroundDescription + ", fname="
-				+ fname + ", lname=" + lname + ", previousIndustry=" + previousIndustry + ", codingExperience="
-				+ codingExperience + ", shirtSize=" + shirtSize + ", cohort=" + cohort + ", userID=" + userID
-				+ ", user=" + user + "]";
+		StringBuilder builder = new StringBuilder();
+		builder.append("Profile [id=");
+		builder.append(id);
+		builder.append(", img=");
+		builder.append(img);
+		builder.append(", backgroundDescription=");
+		builder.append(backgroundDescription);
+		builder.append(", fname=");
+		builder.append(fname);
+		builder.append(", lname=");
+		builder.append(lname);
+		builder.append(", previousIndustry=");
+		builder.append(previousIndustry);
+		builder.append(", codingExperience=");
+		builder.append(codingExperience);
+		builder.append(", shirtSize=");
+		builder.append(shirtSize);
+		builder.append(", cohort=");
+		builder.append(cohort);
+		builder.append("]");
+		return builder.toString();
 	}
-	
-	
+
+	// toString
+
 
 }
