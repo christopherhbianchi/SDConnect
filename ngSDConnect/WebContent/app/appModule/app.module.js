7c813186ee80427f0e9d8a878968457a4f35f1e9
C Bianchi <chrishbianchi@gmail.com>
2017-11-02 15:25:32 -0600

    nav component and html, and congif on app module

--- ngSDConnect/WebContent/app/appModule/app.module.js
+++ ngSDConnect/WebContent/app/appModule/app.module.js
@@ -18,48 +18,8 @@ angular.module("appModule", ['ngRoute', 'ngCookies', 'authModule'])
 			.when('/profile',{
 				template:'<profile></profile>'
 			})
-		
-		
-		
-		
-		
-		
-		
+			.otherwise({
+				template:'<h1>404 - NOT FOUND </h1>'
+			})	
 		
 	})
-=======
-angular.module("appModule", ['ngRoute', "ngCookies", "authModule"]) //<!--this is how you create a module; it is like a jar; it is the entire executable portion that encapsulates the functonality-->
-.config(function($routeProvider){
-	
-	 $routeProvider
-	    .when('/', {
-	    	  template: '<login></login>'  //registration and login
-	    })
-	    .when('/home', {
-	      template : '<home></home>'
-	    })
-	    .when('/profile', {
-	    	template : '<profile></profile>'
-	    })
-	    .when('/alumni', {
-	    	template : '<alumni></alumni>'
-	    })
-	 	.when("/resources", {
-		template: "<resources></resources>"
-	 	})
-	 	.when("/logout", {
-		template: "<logout></logout>"
-	 	})
-	    .otherwise({
-	    template:  '<h1>404 - NOT FOUND </h1>'
-	    })
-});
-
-
-//.when('/todos/:id', {
-//template:"<todo-list></todo-list>"
-//})
-//.when('/todos/notfound', {
-//template:  '<h1>404 - NOT FOUND </h1>'
-//})
->>>>>>> 4975c16204485c65ff63fcb9883f037e0d93f7bf
