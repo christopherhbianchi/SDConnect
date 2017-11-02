49dd67a905ab7f39a750dfab454b90df77b063f1
C Bianchi <chrishbianchi@gmail.com>
2017-11-02 15:20:30 -0600

    nav component and html, and congif on app module

--- ngSDConnect/WebContent/app/appModule/app.module.js
+++ ngSDConnect/WebContent/app/appModule/app.module.js
@@ -1,1 +1,28 @@
-angular.module("appModule", []);
+angular.module("appModule", ['ngRoute', 'ngCookies', 'authModule'])
+	.config(function($routeProvider){
+	
+		$routeProvider
+			.when('/', {
+				template:'<home></home>' //these are the snake case names of the comps
+			})
+			.when('/alumni', {
+				template:'<alumni></alumni>' //these are the snake case names of the comps
+			})
+			.when('/login',{
+				template:'<login></login>'
+			})
+			.when('/resources',{
+				template:'<resources></resources>'
+			})
+			.when('/profile',{
+				template:'<profile></profile>'
+			})
+		
+		
+		
+		
+		
+		
+		
+		
+	})
