92c1a9ee372ba39691acf192c1b7224cc2cef77b
Meira Pentermann <meira@meirapentermann.com>
2017-11-03 11:40:35 -0600

    still working on filter

--- ngSDConnect/WebContent/app/appModule/nocareer.filter.js
+++ ngSDConnect/WebContent/app/appModule/nocareer.filter.js
@@ -1,12 +1,12 @@
 angular.module('appModule')
-	.filter('nocareer' , function(){
+	.filter('nocareer' , function(topicService){
 		return function(topicList){
 			var filtered = [];
 			var foundTag = false;
 			var resumeList = topicService.careerResource('resume');
-			var interviewList = topicService.careerResource('interview');
-			var coverList = topicService.careerResource('cover');
-			var careerList = resumeList.concat(interviewList).concat(coverList);
+//			var interviewList = topicService.careerResource('interview');
+//			var coverList = topicService.careerResource('cover');
+			var careerList = resumeList;
 			console.log(careerList);
 			for(var i=0; i<topicList.length; i++) {
 				for(var j=0; j<careerList.length; j++) {
