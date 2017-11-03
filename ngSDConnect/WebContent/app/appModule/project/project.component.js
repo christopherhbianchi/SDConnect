5c350a7271b3e1421eeaf7af3a365c205508bb70
Meira Pentermann <meira@meirapentermann.com>
2017-11-03 15:12:53 -0600

    adding a project service to index.html

--- ngSDConnect/WebContent/app/appModule/project/project.component.js
+++ ngSDConnect/WebContent/app/appModule/project/project.component.js
@@ -2,8 +2,23 @@ angular.module("appModule")
 	.component("project", {
 		templateUrl: "app/appModule/project/project.component.html",
 		
-		controller: function(){
+		controller: function(projectService, $filter, $location, $routeParams){
 			var vm = this;
+			
+			vm.projectList = [];
+			
+			var getProjects = function(){
+				projectService.index()
+				.then(function(resp){
+					console.log(resp.data);
+					vm.projectList = resp.data;
+				})
+				.catch(function(error){
+					console.log(error);
+				})
+			};
+			
+			getProjects();
 			
 		},
 			
---
+++ ngSDConnect/WebContent/app/appModule/project/project.service.js
@@ -0,0 +1,86 @@
+angular.module('appModule')
+	.factory('projectService', function($http, $filter, $location, authService){
+		var service = {};
+		
+		var checkLogin = function(){
+			var userIdPass = authService.getToken();
+//			return userIdPass.id;
+			return 1;
+		};
+		
+		service.index = function(){
+			var uid = checkLogin();
+			if(isNaN(uid)) {
+				$location.path('/login');
+			}
+			else {
+				return $http({
+					method : 'GET',
+					url : 'rest/projects'
+				})
+			}
+		};
+		
+		service.show = function(pid){
+			var uid = checkLogin();
+			if(isNaN(uid)) {
+				$location.path('/login');
+			}
+			else {
+				return $http({
+					method : 'GET',
+					url : 'rest/projects/' + pid
+				})
+			}
+		};
+		
+		service.create = function(project) {
+			var uid = checkLogin();
+			if(isNaN(uid)) {
+				$location.path('/login');
+			}
+			else {
+				return $http({
+					method : 'POST',
+					url : 'rest/projects',
+					headers : {
+						'Content-Type' : 'application/json'
+					},
+					data : project
+				})
+			}
+		}
+	
+		service.update = function(project) {
+			var pid = project.id;
+			var uid = checkLogin();
+			if(isNaN(uid)) {
+				$location.path('/login');
+			}
+			else {
+				return $http({
+					method : 'PUT',
+					url : 'rest/users/' + uid + 'topics/' + pid,
+					headers : {
+						'Content-Type' : 'application/json'
+					},
+					data : project
+				})
+			}
+		}
+		
+		service.destroy = function(pid) {
+			var uid = checkLogin();
+			if(isNaN(uid)) {
+				$location.path('/login');
+			}
+			else {
+				return $http({
+					method : 'DELETE',
+					url : 'rest/users/' + uid + 'topics/' + pid
+				})
+			}
+		}
+		
+		return service;
+	});
